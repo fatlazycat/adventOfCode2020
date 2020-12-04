67cc609093d7e38311ce5b2259ae89dcb9fbd69f
Graham Berks <graham@fatlazycat.com>
2020-12-04 10:12:35 +0000

    Done day 4

---
+++ app/src/test/kotlin/adventOfCode2020/Day4.kt
@@ -0,0 +1,125 @@
+package adventOfCode2020
+
+import kotlin.test.Test
+
+class Day4 {
+    @Test fun testDay4() {
+        val testData: List<String> = getFileAsListOfLines("/day4")
+        var mapOfData: MutableList<Map<String, String>> = mutableListOf()
+        var entry: MutableMap<String,String> = mutableMapOf()
+
+        for(row in testData) {
+            if(row.isBlank()) {
+                mapOfData.add(entry)
+                entry = mutableMapOf()
+            } else {
+                val items = row.split(" ")
+                for(i in items) {
+                    val mapEntry = i.split(":")
+                    entry[mapEntry[0]] = mapEntry[1]
+                }
+            }
+        }
+
+        val number = mapOfData.map { entry -> isPassportPresent(entry) }.map { i -> if (i) 1 else 0 }.sum()
+        assert(number == 202)
+
+        val number2 = mapOfData.map { entry -> isPassportValid(entry) }.map { i -> if (i) 1 else 0 }.sum()
+        assert(number2 == 137)
+    }
+
+    private fun isPassportPresent(passport: Map<String, String>) : Boolean {
+        when (passport.keys.size) {
+            8 -> {
+                return passport.containsKey("byr") &&
+                        passport.containsKey("iyr") &&
+                        passport.containsKey("eyr") &&
+                        passport.containsKey("hgt") &&
+                        passport.containsKey("hcl") &&
+                        passport.containsKey("ecl") &&
+                        passport.containsKey("pid") &&
+                        passport.containsKey("cid")
+            }
+            7 -> {
+                return passport.containsKey("byr") &&
+                        passport.containsKey("iyr") &&
+                        passport.containsKey("eyr") &&
+                        passport.containsKey("hgt") &&
+                        passport.containsKey("hcl") &&
+                        passport.containsKey("ecl") &&
+                        passport.containsKey("pid")
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+
+    private fun isPassportValid(passport: Map<String, String>) : Boolean {
+        when (passport.keys.size) {
+            8 -> {
+                return passport.containsKey("byr") && passport.getValue("byr").toInt() >= 1920 && passport.getValue("byr").toInt() <= 2002 &&
+                        passport.containsKey("iyr") && passport.getValue("iyr").toInt() >= 2010 && passport.getValue("iyr").toInt() <= 2020 &&
+                        passport.containsKey("eyr") && passport.getValue("eyr").toInt() >= 2020 && passport.getValue("eyr").toInt() <= 2030 &&
+                        passport.containsKey("hgt") && validHeight(passport.getValue("hgt")) &&
+                        passport.containsKey("hcl") && validHairColour(passport.getValue("hcl")) &&
+                        passport.containsKey("ecl") && validEyeColour(passport.getValue("ecl")) &&
+                        passport.containsKey("pid") && validPassportNumber(passport.getValue("pid")) &&
+                        passport.containsKey("cid")
+            }
+            7 -> {
+                return passport.containsKey("byr") && passport.getValue("byr").toInt() >= 1920 && passport.getValue("byr").toInt() <= 2002 &&
+                        passport.containsKey("iyr") && passport.getValue("iyr").toInt() >= 2010 && passport.getValue("iyr").toInt() <= 2020 &&
+                        passport.containsKey("eyr") && passport.getValue("eyr").toInt() >= 2020 && passport.getValue("eyr").toInt() <= 2030 &&
+                        passport.containsKey("hgt") && validHeight(passport.getValue("hgt")) &&
+                        passport.containsKey("hcl") && validHairColour(passport.getValue("hcl")) &&
+                        passport.containsKey("ecl") && validEyeColour(passport.getValue("ecl")) &&
+                        passport.containsKey("pid") && validPassportNumber(passport.getValue("pid"))
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+
+    private fun validPassportNumber(pid: String): Boolean {
+        return pid.matches("([0-9]{9})".toRegex())
+    }
+
+    private fun validEyeColour(ecl: String): Boolean {
+        return ecl in listOf<String>("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+    }
+
+    private fun validHairColour(hcl: String): Boolean {
+        return hcl.matches("#([0-9a-f]{6})".toRegex())
+    }
+
+    private fun validHeight(hgt: String) : Boolean {
+        return when {
+            hgt.endsWith("in") -> {
+                val n = hgt.substring(0, hgt.length-2).toIntOrNull()
+
+                n != null && n >= 59 && n <= 76
+            }
+            hgt.endsWith("cm") -> {
+                val n = hgt.substring(0, hgt.length-2).toIntOrNull()
+
+                n != null && n >= 150 && n <= 193
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+//    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+//    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+//    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+//    hgt (Height) - a number followed by either cm or in:
+//    If cm, the number must be at least 150 and at most 193.
+//    If in, the number must be at least 59 and at most 76.
+//    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+//    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+//    pid (Passport ID) - a nine-digit number, including leading zeroes.
+//    cid (Country ID) - ignored, missing or not.
+}
