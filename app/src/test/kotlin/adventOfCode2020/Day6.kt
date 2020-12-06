24cdcf1be3d235a5689d1a9b62666cbd59f92859
Graham Berks <graham@fatlazycat.com>
2020-12-06 11:43:05 +0000

    Trying out extensions

--- app/src/test/kotlin/adventOfCode2020/Day6.kt
+++ app/src/test/kotlin/adventOfCode2020/Day6.kt
@@ -2,6 +2,9 @@ package adventOfCode2020
 
 import kotlin.test.Test
 
+fun String.distinct() = this.toCharArray().distinct()
+fun String.intersect(other: Iterable<Char>): Set<Char> = this.toCharArray().intersect(other)
+
 class Day6 {
     @Test
     fun testDay6() {
@@ -15,8 +18,8 @@ class Day6 {
         data.map { i -> distinctChars(i).size }.sum()
 
     private fun countUnique2(data: List<List<String>>) = data.map { i ->
-        i.fold(distinctChars(i).toSet()) { acc, item -> item.toCharArray().intersect(acc) }.size
+        i.fold(distinctChars(i).toSet()) { acc, item -> item.intersect(acc) }.size
     }.sum()
 
-    private fun distinctChars(s: List<String>) = s.joinToString("").toCharArray().distinct()
+    private fun distinctChars(s: List<String>) = s.joinToString("").distinct()
 }
