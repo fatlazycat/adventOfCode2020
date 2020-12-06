102e2690249e1b9fcaebb8093ee88d25850fde5c
Graham Berks <graham@fatlazycat.com>
2020-12-06 10:13:29 +0000

    clean code

--- app/src/test/kotlin/adventOfCode2020/Day6.kt
+++ app/src/test/kotlin/adventOfCode2020/Day6.kt
@@ -5,16 +5,16 @@ import kotlin.test.Test
 class Day6 {
     @Test
     fun testDay6() {
-        val testData: List<String> = getFileAsListOfLines("/day6")
+        val data = dataSet(getFileAsListOfLines("/day6"))
 
-        assert(countUnique(testData) == 7110)
-        assert(countUnique2(testData) == 3628)
+        assert(countUnique(data) == 7110)
+        assert(countUnique2(data) == 3628)
     }
 
-    private fun countUnique(testData: List<String>) = dataSet(testData)
-        .map { i -> i.joinToString("").toCharArray().distinct().size }.sum()
+    private fun countUnique(data: List<List<String>>) =
+        data.map { i -> i.joinToString("").toCharArray().distinct().size }.sum()
 
-    private fun countUnique2(testData: List<String>) = dataSet(testData).map { i ->
+    private fun countUnique2(data: List<List<String>>) = data.map { i ->
         i.drop(1).fold(i[0].toCharArray().toSet()) { acc, item -> item.toCharArray().intersect(acc) }.size
     }.sum()
 }
