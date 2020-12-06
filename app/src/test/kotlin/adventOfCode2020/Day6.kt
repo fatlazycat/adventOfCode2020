ffb6d36ad68f9eebcff437965a92759c96f6b082
Graham Berks <graham@fatlazycat.com>
2020-12-06 08:10:16 +0000

    Done day 6

---
+++ app/src/test/kotlin/adventOfCode2020/Day6.kt
@@ -0,0 +1,39 @@
+package adventOfCode2020
+
+import kotlin.test.Test
+
+class Day6 {
+    @Test
+    fun testDay6() {
+        val testData: List<String> = getFileAsListOfLines("/day6")
+
+        assert(countUnique(testData) == 7110)
+        assert(countUnique2(testData) == 3628)
+    }
+
+    private fun countUnique(testData: List<String>): Int {
+        return dataSet(splitPoints(testData), testData)
+            .map { i -> i.joinToString("") }
+            .map { i -> i.toCharArray().distinct() }
+            .map { i -> i.size }.sum()
+    }
+
+    private fun countUnique2(testData: List<String>): Int {
+        return dataSet(splitPoints(testData), testData)
+            .map { i ->
+                i.drop(1).fold(i[0].toCharArray().toSet()) { acc, item -> item.toCharArray().intersect(acc) }.size
+            }.sum()
+    }
+
+    private fun splitPoints(testData: List<String>) =
+        testData.mapIndexed { index, item -> if (item.isNullOrEmpty()) index else -1 }.filter { it != -1 }
+
+    private fun dataSet(
+        splitPoints: List<Int>,
+        testData: List<String>
+    ) = (listOf(-1) + splitPoints + testData.size)
+        .windowed(2, 1)
+        .map { (start, end) -> testData.subList(start + 1, end) }
+        .map { i -> i.map { j -> j.split(" ") }.flatten() }
+
+}
