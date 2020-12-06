ac0d80fea13e581c8ac8286bbcde3081a448e915
Graham Berks <graham@fatlazycat.com>
2020-12-06 11:47:00 +0000

    Clean code

--- app/src/test/kotlin/adventOfCode2020/Day6.kt
+++ app/src/test/kotlin/adventOfCode2020/Day6.kt
@@ -2,7 +2,7 @@ package adventOfCode2020
 
 import kotlin.test.Test
 
-fun String.distinct() = this.toCharArray().distinct()
+fun String.distinct(): List<Char> = this.toCharArray().distinct()
 fun String.intersect(other: Iterable<Char>): Set<Char> = this.toCharArray().intersect(other)
 fun List<String>.distinct() = this.joinToString("").distinct()
 
@@ -15,10 +15,11 @@ class Day6 {
         assert(countUnique2(data) == 3628)
     }
 
-    private fun countUnique(data: List<List<String>>) =
-        data.map { i -> i.distinct().size }.sum()
+    private fun countUnique(data: List<List<String>>) = data.map { i -> i.distinct().size }.sum()
 
     private fun countUnique2(data: List<List<String>>) = data.map { i ->
-        i.fold(i.distinct().toSet()) { acc, item -> item.intersect(acc) }.size
+        i.fold(
+            i.distinct().toSet()
+        ) { acc, item -> item.intersect(acc) }.size
     }.sum()
 }
