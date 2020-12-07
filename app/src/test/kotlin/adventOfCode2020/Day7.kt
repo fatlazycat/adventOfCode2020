897116d49cfdd1bc4bfbfcd1c9b09a17fc8be476
Graham Berks <graham@fatlazycat.com>
2020-12-07 20:22:27 +0000

    Rationalized parsing ( well sort of ... )

--- app/src/test/kotlin/adventOfCode2020/Day7.kt
+++ app/src/test/kotlin/adventOfCode2020/Day7.kt
@@ -2,6 +2,12 @@ package adventOfCode2020
 
 import kotlin.test.Test
 
+fun CharSequence.splitIgnoreEmpty(vararg delimiters: String): List<String> {
+    return this.split(*delimiters).filter {
+        it.isNotEmpty()
+    }
+}
+
 class Day7 {
     private val shinyGold = "shiny gold"
 
@@ -11,7 +17,7 @@ class Day7 {
         val mapData = makeMap(data)
         val numberOfBags = mapData.keys.map { bagContainsGold(it, mapData) }
 
-        assert(numberOfBags.map { if(it) 1 else 0 }.sum() == 370)
+        assert(numberOfBags.map { if (it) 1 else 0 }.sum() == 370)
     }
 
     @Test
@@ -23,29 +29,32 @@ class Day7 {
     }
 
     private fun bagCount(bag: String, mapData: Map<String, List<Pair<String, Int>>>): Int {
-        return mapData[bag]?.map { i -> (bagCount(i.first, mapData)+1) * i.second }?.sum() ?: 0
+        return mapData[bag]?.map { i -> (bagCount(i.first, mapData) + 1) * i.second }?.sum() ?: 0
     }
 
     private fun bagContainsGold(bag: String, mapData: Map<String, List<Pair<String, Int>>>): Boolean {
-        return mapData[bag]?.map { i -> i.first == shinyGold || bagContainsGold(i.first, mapData) }?.contains(true) ?: false
+        return mapData[bag]?.map { i -> i.first == shinyGold || bagContainsGold(i.first, mapData) }?.contains(true)
+            ?: false
     }
 
-    private fun makeMap(l: List<String>) : Map<String, List<Pair<String, Int>>> {
-        return l.map{ item -> makeEntry(item) }.toMap()
+    private fun makeMap(l: List<String>): Map<String, List<Pair<String, Int>>> {
+        return l.map { item -> makeEntry(item) }.toMap()
     }
 
-    private fun makeEntry(item: String) : Pair<String, List<Pair<String, Int>>> {
-        val partsNormalized = item.replace(".", "")
-            .replace("no other", "0 blank")
-            .replace(" bags", "")
-            .replace(" bag", "")
-        val parts = partsNormalized.split(" contain ")
-        val entries = parts[1].split("\\ contain\\ ".toRegex())
-        val bagsStrings = entries[0].split(",\\ ".toRegex())
-        val bagsWithEmpty = bagsStrings.map{ i-> Pair(i.substring(2), i[0].toString().toInt())}
-        val bags = if (bagsWithEmpty.size == 1 && bagsWithEmpty[0] == Pair("blank", 0)) listOf<Pair<String, Int>>() else bagsWithEmpty
-        val bagsProcessed = bags.map{ i -> Pair(i.first.trim(), i.second)}
+    private fun makeEntry(item: String): Pair<String, List<Pair<String, Int>>> {
+        val regex = """([a-z0-9 ]*)(?: bags contain )([a-z0-9\s].*,)*?([a-z0-9\s]*\.|no\sother\sbags\.)""".toRegex()
 
-        return parts[0].replace(" bags", "") to bagsProcessed
+        regex.find(item)!!.groupValues.let { (_, bag, body, end) ->
+            return Pair(bag, makeListEntry(body) + makeListEntry(end))
+        }
+    }
+
+    private fun makeListEntry(s: String) : List<Pair<String, Int>> {
+        return if (s == "no other bags.")
+            listOf()
+        else {
+            val parts = s.splitIgnoreEmpty(",", ".").map { it.trim() }
+            parts.map { i -> Pair(i.substring(2, i.length-4).trim(), i[0].toString().toInt()) }
+        }
     }
 }
