9a99ff13caf0a160e1166d369b2fdb76d575ede7
Graham Berks <graham@fatlazycat.com>
2020-12-10 12:30:46 +0000

    Day 10 done but head still hurts

--- app/src/test/kotlin/adventOfCode2020/Day10.kt
+++ app/src/test/kotlin/adventOfCode2020/Day10.kt
@@ -8,66 +8,40 @@ class Day10 {
         val lines = getFileAsListOfLines("/day10")
         val data = lines.map { it.toInt() }
         val sortedData = data.sorted()
-        val d = findChain(sortedData + listOf(sortedData.maxOrNull()!!+3))
+        val d = findChain(sortedData + listOf(sortedData.maxOrNull()!! + 3))
 
         assert(d.one * d.three == 1998)
     }
 
-    @Test fun testPart2() {
-        val lines = getFileAsListOfLines("/day10test2")
+    @Test
+    fun testPart2() {
+        val lines = getFileAsListOfLines("/day10")
         val data = lines.map { it.toInt() }
         val sortedData = data.sorted()
-        val total = findAllChains(sortedData + listOf(sortedData.maxOrNull()!!+3))
-        println(total)
+        val total = getCombinations(sortedData + (sortedData.maxOrNull()!! + 3))
+
+        assert(total == 347250213298688L)
     }
 
-    private fun findAllChains(l: List<Int>) : Int {
-        val finalNum = l.last()
-        val body = l.take(l.size-1)
-        var total = mutableListOf<List<Int>>()
-
-        val indices = (0..body.size-1).windowed(3)
-
-        indices.map { i ->
-            val head = body.subList(0, i[0])
-            val tail = body.subList(i[2], body.size)
-
-            if(findChain(head + body[i[0]] + tail + finalNum).last != -1)
-                total.add(head + body[i[0]] + tail + finalNum)
-
-            if(findChain(head + body[i[1]] + tail + finalNum).last != -1)
-                total.add(head + body[i[1]] + tail + finalNum)
-
-            if(findChain(head + body[i[2]] + tail + finalNum).last != -1)
-                total.add(head + body[i[2]] + tail + finalNum)
-
-            if(findChain(head + body[i[0]] + body[i[1]] + tail + finalNum).last != -1)
-                total.add(head + body[i[0]] + body[i[1]] + tail + finalNum)
-
-            if(findChain(head + body[i[1]] + body[i[2]] + tail + finalNum).last != -1)
-                total.add(head + body[i[1]] + body[i[2]] + tail + finalNum)
-
-            if(findChain(head + body[i[0]] + body[i[2]] + tail + finalNum).last != -1)
-                total.add(head + body[i[0]] + body[i[2]] + tail + finalNum)
-
-            if(findChain(head + body[i[0]] + body[i[1]] + body[i[2]] + tail + finalNum).last != -1)
-                total.add(head + body[i[0]] + body[i[1]] + body[i[2]] + tail + finalNum)
-        }
-
-        return total.distinct().size
+    private fun getCombinations(l: List<Int>): Long {
+        return l.fold(listOf(Pair(0, 1L))) { acc, i ->
+            val possibleCombinations = acc.sumOf { p -> if(i - p.first <=3) p.second else 0L }
+            acc.takeLast(2) + Pair(i, possibleCombinations)
+        }.last().second
     }
 
-    private fun findChain(l: List<Int>) : Data {
-        return l.fold(Data(0,0,0,0)) { acc, i ->
+
+    private fun findChain(l: List<Int>): Data {
+        return l.fold(Data(0, 0, 0, 0)) { acc, i ->
             processEntry(i, acc)
         }
     }
 
-    private fun processEntry(i: Int, acc: Data) : Data {
+    private fun processEntry(i: Int, acc: Data): Data {
         return when (i) {
             acc.last + 1 -> Data(i, acc.one + 1, acc.two, acc.three)
-            acc.last + 2 -> Data(i, acc.one, acc.two+1, acc.three)
-            acc.last + 3 -> Data(i, acc.one, acc.two, acc.three+1)
+            acc.last + 2 -> Data(i, acc.one, acc.two + 1, acc.three)
+            acc.last + 3 -> Data(i, acc.one, acc.two, acc.three + 1)
             else -> Data(-1, -1, -1, -1)
         }
     }
