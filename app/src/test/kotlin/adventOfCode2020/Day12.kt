f4051ff0fecd89df9bf43c1d3989ea84f950c748
Graham Berks <graham@fatlazycat.com>
2020-12-12 09:42:02 +0000

    Done Day 12 part 2

--- app/src/test/kotlin/adventOfCode2020/Day12.kt
+++ app/src/test/kotlin/adventOfCode2020/Day12.kt
@@ -14,6 +14,15 @@ class Day12 {
     }
 
     @Test
+    fun testPart2() {
+        val lines: List<String> = getFileAsListOfLines("/day12")
+        val directions = mapData(lines)
+        val result = followPathWaypoint(directions)
+
+        assert(result == 66614)
+    }
+
+    @Test
     fun testChangeDirection() {
         val current = Path(Pair(0,0), Direction.EAST)
         assert( changeDirection(current, Pair(Direction.LEFT, 90)) == Path(Pair(0,0), Direction.NORTH))
@@ -27,10 +36,10 @@ class Day12 {
     private fun followPath(l: List<Pair<Direction, Int>>) : Int {
         val newPosition = l.fold(Path(Pair(0,0), Direction.EAST)){ acc, i ->
             when(i.first) {
-                Direction.NORTH -> Path(Pair(acc.current.first + i.second, acc.current.second), acc.direction)
-                Direction.SOUTH -> Path(Pair(acc.current.first - i.second, acc.current.second), acc.direction)
-                Direction.EAST -> Path(Pair(acc.current.first, acc.current.second + i.second), acc.direction)
-                Direction.WEST -> Path(Pair(acc.current.first, acc.current.second - i.second), acc.direction)
+                Direction.NORTH -> Path(Pair(acc.position.first + i.second, acc.position.second), acc.direction)
+                Direction.SOUTH -> Path(Pair(acc.position.first - i.second, acc.position.second), acc.direction)
+                Direction.EAST -> Path(Pair(acc.position.first, acc.position.second + i.second), acc.direction)
+                Direction.WEST -> Path(Pair(acc.position.first, acc.position.second - i.second), acc.direction)
                 Direction.LEFT -> changeDirection(acc, i)
                 Direction.RIGHT -> changeDirection(acc, i)
                 Direction.FORWARD -> moveForward(acc, i)
@@ -38,30 +47,87 @@ class Day12 {
             }
         }
 
-        return newPosition.current.first.absoluteValue + newPosition.current.second.absoluteValue
+        return newPosition.position.first.absoluteValue + newPosition.position.second.absoluteValue
+    }
+
+    private fun followPathWaypoint(l: List<Pair<Direction, Int>>) : Int {
+        val newPosition = l.fold(PathWaypoint(Pair(0,0), Pair(1, 10))){ acc, i ->
+            when(i.first) {
+                Direction.NORTH -> PathWaypoint(acc.position, Pair(acc.waypoint.first + i.second, acc.waypoint.second))
+                Direction.SOUTH -> PathWaypoint(acc.position, Pair(acc.waypoint.first - i.second, acc.waypoint.second))
+                Direction.EAST -> PathWaypoint(acc.position, Pair(acc.waypoint.first, acc.waypoint.second + i.second))
+                Direction.WEST -> PathWaypoint(acc.position, Pair(acc.waypoint.first, acc.waypoint.second - i.second))
+                Direction.LEFT -> changeDirectionWaypoint(acc, i)
+                Direction.RIGHT -> changeDirectionWaypoint(acc, i)
+                Direction.FORWARD -> moveForwardWaypoint(acc, i)
+                else -> throw UnsupportedOperationException()
+            }
+        }
+
+        return newPosition.position.first.absoluteValue + newPosition.position.second.absoluteValue
     }
 
     private fun moveForward(current: Path, instruction: Pair<Direction, Int>): Path {
         val newPosition = when(current.direction) {
-            Direction.NORTH -> Path(Pair(current.current.first + instruction.second, current.current.second), current.direction)
-            Direction.EAST -> Path(Pair(current.current.first, current.current.second + instruction.second), current.direction)
-            Direction.SOUTH -> Path(Pair(current.current.first - instruction.second, current.current.second), current.direction)
-            Direction.WEST -> Path(Pair(current.current.first, current.current.second - instruction.second), current.direction)
+            Direction.NORTH -> Path(Pair(current.position.first + instruction.second, current.position.second), current.direction)
+            Direction.EAST -> Path(Pair(current.position.first, current.position.second + instruction.second), current.direction)
+            Direction.SOUTH -> Path(Pair(current.position.first - instruction.second, current.position.second), current.direction)
+            Direction.WEST -> Path(Pair(current.position.first, current.position.second - instruction.second), current.direction)
             else -> throw UnsupportedOperationException()
         }
 
         return newPosition
     }
 
-    private fun changeDirection(current: Path, instruction: Pair<Direction, Int>): Path {
+    private fun moveForwardWaypoint(current: PathWaypoint, instruction: Pair<Direction, Int>): PathWaypoint {
+        return PathWaypoint(
+            Pair(current.position.first + current.waypoint.first * instruction.second,
+            current.position.second + current.waypoint.second * instruction.second),
+            current.waypoint)
+    }
+
+    private fun changeDirection(position: Path, instruction: Pair<Direction, Int>): Path {
         val turns = instruction.second / 90
         val newDirection = when(instruction.first) {
-            Direction.LEFT -> Direction.from((4 + current.direction.num - turns) % 4)
-            Direction.RIGHT -> Direction.from((current.direction.num + turns) % 4)
+            Direction.LEFT -> Direction.from((4 + position.direction.num - turns) % 4)
+            Direction.RIGHT -> Direction.from((position.direction.num + turns) % 4)
             else -> throw UnsupportedOperationException()
         }
 
-        return Path(current.current, newDirection)
+        return Path(position.position, newDirection)
+    }
+
+    @Test
+    fun testChangeDirectionWaypoint() {
+        assert( changeDirectionWaypoint(PathWaypoint(Pair(0,0), Pair(4,10)), Pair(Direction.RIGHT, 90)) == PathWaypoint(Pair(0,0), Pair(-10,4)))
+    }
+
+    private fun changeDirectionWaypoint(position: PathWaypoint, instruction: Pair<Direction, Int>): PathWaypoint {
+        val turns = instruction.second / 90
+        return  when(instruction.first) {
+            Direction.LEFT -> {
+                when (turns) {
+                    0 -> PathWaypoint(position.position, position.waypoint)
+                    1 -> PathWaypoint(position.position, Pair(position.waypoint.second, position.waypoint.first * -1))
+                    2 -> PathWaypoint(
+                        position.position,
+                        Pair(position.waypoint.first * -1, position.waypoint.second * -1)
+                    )
+                    3 -> PathWaypoint(position.position, Pair(position.waypoint.second * -1, position.waypoint.first))
+                    else -> throw UnsupportedOperationException()
+                }
+            }
+            Direction.RIGHT -> {
+                when(turns) {
+                    0 -> PathWaypoint(position.position, position.waypoint)
+                    1 -> PathWaypoint(position.position, Pair(position.waypoint.second * -1, position.waypoint.first))
+                    2 -> PathWaypoint(position.position, Pair(position.waypoint.first * -1, position.waypoint.second * -1))
+                    3 -> PathWaypoint(position.position, Pair(position.waypoint.second, position.waypoint.first * -1))
+                    else -> throw UnsupportedOperationException()
+                }
+            }
+            else -> throw UnsupportedOperationException()
+        }
     }
 
     private fun mapData(l: List<String>) : List<Pair<Direction, Int>> {
@@ -81,7 +147,9 @@ class Day12 {
         }
     }
 
-    private data class Path(val current: Pair<Int, Int>, val direction: Direction)
+    private data class Path(val position: Pair<Int, Int>, val direction: Direction)
+
+    private data class PathWaypoint(val position: Pair<Int, Int>, val waypoint: Pair<Int, Int>)
 
     enum class Direction(val num: Int) {
         NORTH(0),
