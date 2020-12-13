d6314f4e8a98402f4f4c3da5e5740a5dd75bc7ff
Graham Berks <graham@fatlazycat.com>
2020-12-13 11:36:01 +0000

    Done Day 13 Part 2

--- app/src/test/kotlin/adventOfCode2020/Day13.kt
+++ app/src/test/kotlin/adventOfCode2020/Day13.kt
@@ -1,6 +1,7 @@
 package adventOfCode2020
 
 import org.junit.Test
+import java.lang.Long.max
 import kotlin.math.floor
 
 
@@ -14,6 +15,50 @@ class Day13 {
         val result = bus.first * bus.second
 
         assert(result == 246)
+    }
+
+    @Test
+    fun testPart2() {
+        val lines: List<String> = getFileAsListOfLines("/day13")
+        val buses = lines[1].split(",").mapIndexed{ index, b -> Pair(index, b) }
+            .filter { it.second != "x" }.map { p -> Pair(p.first, p.second.toLong()) }
+        val timestamp = busesLineUp(buses)
+
+        assert(timestamp == 939490236001473)
+    }
+
+    @Test
+    fun testPart2test() {
+        val lines: List<String> = getFileAsListOfLines("/day13test")
+        val buses = lines[1].split(",").mapIndexed{ index, b -> Pair(index, b) }
+            .filter { it.second != "x" }.map { p -> Pair(p.first, p.second.toLong()) }
+        val timestamp = busesLineUp(buses)
+
+        assert(timestamp == 1068781L)
+    }
+
+    private fun busesLineUp(l: List<Pair<Int, Long>>) : Long {
+        val stopDivisor = l.map { it.second }.fold(1L){acc, x -> acc * x}
+        val startTime = l.map { it.second }.maxOrNull()!!.toLong()
+
+        return atTimestamp(startTime, l, stopDivisor)
+    }
+
+    private tailrec fun atTimestamp(time: Long, l: List<Pair<Int, Long>>, stopDivisor: Long): Long {
+        val divisor = lookForDivisor(time, 1, l)
+
+        return if (divisor == stopDivisor)
+            time
+        else
+            atTimestamp(time + divisor, l, stopDivisor)
+    }
+
+    private tailrec fun lookForDivisor(time: Long, step: Long, l: List<Pair<Int, Long>>): Long {
+        return when {
+            l.isEmpty() -> return step
+            (time + l.first().first) % l.first().second != 0L -> step
+            else -> lookForDivisor(time, max(step, step * l.first().second), l.drop(1))
+        }
     }
 
     private fun nearestLowMultiple(x: Double, m: Double) = if (m == 0.0) x else floor(x / m) * m
