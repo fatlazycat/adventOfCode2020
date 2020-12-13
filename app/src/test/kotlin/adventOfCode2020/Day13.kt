9eb6daaf925565ebea9cadae0ad2e0ac0b6f323f
Graham Berks <graham@fatlazycat.com>
2020-12-13 13:58:32 +0000

    My solution stumbled upon fact all ids are odd, added solution stepping by LCM

--- app/src/test/kotlin/adventOfCode2020/Day13.kt
+++ app/src/test/kotlin/adventOfCode2020/Day13.kt
@@ -6,6 +6,9 @@ import kotlin.math.floor
 
 
 class Day13 {
+
+    // Part 1
+
     @Test
     fun testPart1() {
         val lines: List<String> = getFileAsListOfLines("/day13")
@@ -17,12 +20,27 @@ class Day13 {
         assert(result == 246)
     }
 
+    private fun nearestLowMultiple(x: Double, m: Double) = if (m == 0.0) x else floor(x / m) * m
+
+    private fun nearestBus(timestamp: Int, buses: List<Int>): Pair<Int, Int> {
+        return buses.map { m ->
+            val nearest = nearestLowMultiple(timestamp.toDouble(), m.toDouble()).toInt()
+
+            if (nearest != timestamp)
+                Pair(m, (nearest + m) - timestamp)
+            else
+                Pair(m, 0)
+        }.minByOrNull { it.second }!!
+    }
+
+    // Part 2
+
     @Test
     fun testPart2() {
         val lines: List<String> = getFileAsListOfLines("/day13")
         val buses = lines[1].split(",").mapIndexed{ index, b -> Pair(index, b) }
             .filter { it.second != "x" }.map { p -> Pair(p.first, p.second.toLong()) }
-        val timestamp = busesLineUp(buses)
+        val timestamp = busesLineUpOddIds(buses)
 
         assert(timestamp == 939490236001473)
     }
@@ -32,12 +50,12 @@ class Day13 {
         val lines: List<String> = getFileAsListOfLines("/day13test")
         val buses = lines[1].split(",").mapIndexed{ index, b -> Pair(index, b) }
             .filter { it.second != "x" }.map { p -> Pair(p.first, p.second.toLong()) }
-        val timestamp = busesLineUp(buses)
+        val timestamp = busesLineUpOddIds(buses)
 
         assert(timestamp == 1068781L)
     }
 
-    private fun busesLineUp(l: List<Pair<Int, Long>>) : Long {
+    private fun busesLineUpOddIds(l: List<Pair<Int, Long>>) : Long {
         val stopDivisor = l.map { it.second }.fold(1L){acc, x -> acc * x}
         val startTime = l.map { it.second }.maxOrNull()!!.toLong()
 
@@ -61,16 +79,54 @@ class Day13 {
         }
     }
 
-    private fun nearestLowMultiple(x: Double, m: Double) = if (m == 0.0) x else floor(x / m) * m
+    // Part 2 all number
 
-    private fun nearestBus(timestamp: Int, buses: List<Int>): Pair<Int, Int> {
-        return buses.map { m ->
-                val nearest = nearestLowMultiple(timestamp.toDouble(), m.toDouble()).toInt()
+    @Test
+    fun testPart2AllNum() {
+        val lines: List<String> = getFileAsListOfLines("/day13")
+        val buses = lines[1].split(",").mapIndexed{ index, b -> Pair(index, b) }
+            .filter { it.second != "x" }.map { p -> Pair(p.first, p.second.toLong()) }
+        val timestamp = busesLineUpAllNum(buses)
 
-                if (nearest != timestamp)
-                    Pair(m, (nearest + m) - timestamp)
-                else
-                    Pair(m, 0)
-            }.minByOrNull { it.second }!!
+        assert(timestamp == 939490236001473)
     }
+
+    @Test
+    fun testPart2testAllNum() {
+        val lines: List<String> = getFileAsListOfLines("/day13test")
+        val buses = lines[1].split(",").mapIndexed{ index, b -> Pair(index, b) }
+            .filter { it.second != "x" }.map { p -> Pair(p.first, p.second.toLong()) }
+        val timestamp = busesLineUpAllNum(buses)
+
+        assert(timestamp == 1068781L)
+    }
+
+    private fun busesLineUpAllNum(l: List<Pair<Int, Long>>) : Long {
+        val stopDivisor = l.map { it.second }.fold(1L){acc, x -> acc * x}
+        val startTime = l.map { it.second }.maxOrNull()!!.toLong()
+
+        return atTimestampAllNum(startTime, l, stopDivisor)
+    }
+
+    private tailrec fun atTimestampAllNum(time: Long, l: List<Pair<Int, Long>>, stopDivisor: Long): Long {
+        val step = lookForMultiplierAllNum(time, listOf(1), l)
+
+        return if (step == -1L)
+            time
+        else
+            atTimestampAllNum(time + step, l, stopDivisor)
+    }
+    private tailrec fun lookForMultiplierAllNum(time: Long, matched: List<Long>, l: List<Pair<Int, Long>>): Long {
+        return when {
+            l.isEmpty() -> return -1
+            (time + l.first().first) % l.first().second != 0L -> lcm(matched)
+            else -> lookForMultiplierAllNum(time, matched + l.first().second, l.drop(1))
+        }
+    }
+
+    private fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+
+    private fun gcd(numbers: List<Long>): Long = numbers.fold(0L) { x, y -> gcd(x, y) }
+
+    private fun lcm(numbers: List<Long>): Long = numbers.fold(1L) { x, y -> x * (y / gcd(x, y)) }
 }
