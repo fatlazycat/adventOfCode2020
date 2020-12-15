e37d455d94b53a1a7f9c93d6c8cad81b1f06512e
Graham Berks <graham@fatlazycat.com>
2020-12-15 10:05:23 +0000

    Day 15 complete - eeewww imperative code

--- app/src/test/kotlin/adventOfCode2020/Day15.kt
+++ app/src/test/kotlin/adventOfCode2020/Day15.kt
@@ -12,25 +12,26 @@ class Day15 {
     private val testData6 = listOf(3,2,1)
     private val testData7 = listOf(3,1,2)
 
-//    Given the starting numbers 1,3,6, the 2020th number spoken is 436.
-//    Given the starting numbers 1,3,2, the 2020th number spoken is 1.
-//    Given the starting numbers 2,1,3, the 2020th number spoken is 10.
-//    Given the starting numbers 1,2,3, the 2020th number spoken is 27.
-//    Given the starting numbers 2,3,1, the 2020th number spoken is 78.
-//    Given the starting numbers 3,2,1, the 2020th number spoken is 438.
-//    Given the starting numbers 3,1,2, the 2020th number spoken is 1836.
-
     @Test
     fun testPart1() {
-        println(number2020(testData1))
+        assert(number2020(testData1) == 436)
+        assert(number2020(testData2) == 1)
+        assert(number2020(testData3) == 10)
+        assert(number2020(testData4) == 27)
+        assert(number2020(testData5) == 78)
+        assert(number2020(testData6) == 438)
+        assert(number2020(testData7) == 1836)
+        assert(number2020(data) == 959)
+    }
+
+    @Test
+    fun testPart2() {
+        assert(numberLarge2020(testData1) == 175594)
+        assert(numberLarge2020(data) == 116590)
     }
 
     private fun number2020(startingNumbers: List<Int>): Int {
-        val numbers = (3 until 2020).map { i->
-            Pair(i, startingNumbers[i.rem(3)])
-        }
-
-        val result = numbers.fold(startingNumbers){ acc, p ->
+        val result = (startingNumbers.size until 2020).fold(startingNumbers){ acc, _ ->
             val toFind = acc.last()
             val foundIndex = acc.lastIndexOf(toFind)
             val prevIndex = acc.dropLast(1).lastIndexOf(toFind)
@@ -40,9 +41,48 @@ class Day15 {
             else if(foundIndex != -1 && prevIndex == -1)
                 acc + 0
             else
-                acc + (p.first-foundIndex)
+                acc + (foundIndex-prevIndex)
         }
 
         return result.last()
+    }
+
+    private fun numberLarge2020(startingNumbers: List<Int>): Int {
+        val m = startingNumbers.mapIndexed{ index, n -> Pair(n, Pair(index, -1)) }.toMap(mutableMapOf())
+
+        return process(startingNumbers.last(), m, startingNumbers.size)
+    }
+
+    private fun process(lastNum: Int, locations: MutableMap<Int, Pair<Int, Int>>, startingNumber: Int) : Int {
+        var toFind = lastNum
+
+        for(i in (startingNumber until 30000000)) {
+            val indicies = locations[toFind]
+
+            if(indicies == null) {
+                locations[toFind] = Pair(i, -1)
+            }
+            else if (indicies.second == -1) {
+                toFind = 0
+                val indiciesOfZero = locations[0]!!
+                val maxIndexOfZero = maxOf(indiciesOfZero.first, indiciesOfZero.second)
+                locations[0] = Pair(maxIndexOfZero, i)
+            }
+            else {
+                val foundIndex = indicies.second
+                val prevIndex = indicies.first
+                toFind = foundIndex - prevIndex
+                val newIndicies = locations[toFind]
+
+                if(newIndicies == null)
+                    locations[toFind] = Pair(i, -1)
+                else if(newIndicies.second == -1)
+                    locations[toFind] = Pair(newIndicies.first, i)
+                else
+                    locations[toFind] = Pair(newIndicies.second, i)
+            }
+        }
+
+        return toFind
     }
 }
