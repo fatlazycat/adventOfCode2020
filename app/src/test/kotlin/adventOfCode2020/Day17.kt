8dcdd45776797d21838910353591afc5e57143a8
Graham Berks <graham@fatlazycat.com>
2020-12-17 07:25:06 +0000

    Day 17

---
+++ app/src/test/kotlin/adventOfCode2020/Day17.kt
@@ -0,0 +1,21 @@
+package adventOfCode2020
+
+import org.junit.Test
+
+class Day17 {
+    val testData = listOf(".#.", "..#", "###")
+    val data = listOf(
+        "#.##....",
+        ".#.#.##.",
+        "###.....",
+        "....##.#",
+        "#....###",
+        ".#.#.#..",
+        ".##...##",
+        "#..#.###")
+
+    @Test
+    fun testPart1() {
+//        val data = processInput("/day16rules", "/day16ticket", "/day17test")
+    }
+}
