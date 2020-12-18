edb4a22fd5768bae91804969ea6e10d7f909d942
Graham Berks <graham@fatlazycat.com>
2020-12-18 13:44:08 +0000

    Implemented Shunting Yards - Dijkstra

--- app/src/test/kotlin/adventOfCode2020/Day18.kt
+++ app/src/test/kotlin/adventOfCode2020/Day18.kt
@@ -1,6 +1,8 @@
 package adventOfCode2020
 
 import org.junit.Test
+import java.lang.UnsupportedOperationException
+import java.util.*
 
 class Day18 {
     @Test
@@ -30,7 +32,129 @@ class Day18 {
         val results = lines.map{ evalPlusFirst(it) }
         assert(results.sum() == 693942L)
     }
+
+    @Test
+    fun testPart1Shunt() {
+        val lines: List<String> = getFileAsListOfLines("/day18")
+        val results = lines.map { l -> evaluateRPN(shuntingYards(splitIntoToken(l), equal)) }
+        assert(results.sum() == 11004703763391)
+    }
+
+    @Test
+    fun testPart2Shunt() {
+        val lines: List<String> = getFileAsListOfLines("/day18")
+        val results = lines.map { l -> evaluateRPN(shuntingYards(splitIntoToken(l), plusGreaterThanMultiply)) }
+        assert(results.sum() == 290726428573651)
+    }
+
+    private val allOperators = listOf("*", "/", "+", "-")
+    private val normal = mapOf("*" to 3, "/" to 3, "+" to 2, "-" to 2, "(" to 4, ")" to 4)
+    private val equal = mapOf("*" to 2, "/" to 2, "+" to 2, "-" to 2, "(" to 4, ")" to 4)
+    private val plusGreaterThanMultiply = mapOf("*" to 2, "/" to 2, "+" to 3, "-" to 3, "(" to 4, ")" to 4)
+
+    private fun shuntingYards(tokens: List<String>, precedence: Map<String, Int>) : List<String> {
+        val outputQueue: Queue<String> = LinkedList(listOf())
+        val operatorStack: Stack<String> = Stack()
+
+        tokens.forEach{ t->
+           if (t.toLongOrNull() != null) {
+               outputQueue.add(t)
+           }
+           else if (allOperators.contains(t)) {
+               while (
+                   !operatorStack.isEmpty() &&
+                   precedence[operatorStack.peek()!!]!! >= precedence[t]!! &&
+                   operatorStack.peek() != "("
+               ) {
+                    outputQueue.add(operatorStack.pop())
+               }
+               operatorStack.push(t)
+           }
+           else if (t == "(") {
+               operatorStack.push("(")
+           }
+           else if (t == ")") {
+               while (
+                   operatorStack.peek()!! != "("
+               ) {
+                   outputQueue.add(operatorStack.pop())
+               }
+               if (operatorStack.peek()!! == "(") {
+                   operatorStack.pop()
+               }
+           }
+        }
+        while(!operatorStack.isEmpty())
+            outputQueue.add(operatorStack.pop())
+
+        return outputQueue.toList()
+    }
+
+    private fun evaluateRPN(tokens: List<String>): Long {
+        return tokens.fold(Stack<Long>()) { acc, t ->
+            if (allOperators.contains(t)) {
+                val n1 = acc.pop()!!
+                val n2 = acc.pop()!!
+                acc.push(performOperation(t, n1, n2))
+                acc
+            }
+            else {
+                acc.push(t.toLong())
+                acc
+            }
+        }.pop()!!
+    }
+
+    private fun performOperation(operation: String, n1: Long, n2: Long): Long {
+        return when (operation) {
+            "+" -> n1 + n2
+            "-" -> n1 - n2
+            "*" -> n1 * n2
+            "/" -> n1 / n2
+            else -> throw UnsupportedOperationException()
+        }
+    }
+
+    private fun splitIntoToken(s: String): List<String> {
+        val result = s.fold(Pair(listOf<String>(), "")) { acc, c ->
+            when (c) {
+                ' ' -> acc
+                '(' -> processLastEntryInAccumulator(acc, '(')
+                ')' -> processLastEntryInAccumulator(acc, ')')
+                '*' -> processLastEntryInAccumulator(acc, '*')
+                '+' -> processLastEntryInAccumulator(acc, '+')
+                else -> {
+                    Pair(acc.first, acc.second + c)
+                }
+            }
+        }
+
+        return (result.first + result.second).filter { it != ""}
+    }
+
+    private fun processLastEntryInAccumulator(p: Pair<List<String>, String>, c: Char): Pair<List<String>, String> {
+        return Pair(p.first + p.second + c.toString(), "")
+    }
 }
+
+class Stack<T>{
+    private val elements: MutableList<T> = mutableListOf()
+    fun isEmpty() = elements.isEmpty()
+    fun count() = elements.size
+    fun push(item: T) = elements.add(item)
+    fun pop() : T? {
+        val item = elements.lastOrNull()
+        if (!isEmpty()){
+            elements.removeAt(elements.size -1)
+        }
+        return item
+    }
+    fun peek() : T? = elements.lastOrNull()
+
+    override fun toString(): String = elements.toString()
+}
+
+fun <T> Stack<T>.push(items: Collection<T>) = items.forEach { this.push(it) }
 
 const val ZERO = '0'.toInt()
 const val NINE = '9'.toInt()
