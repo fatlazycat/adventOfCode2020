56b5326a99874e3eee4c141b83128e39d4e19ae1
Graham Berks <graham@fatlazycat.com>
2020-12-22 11:58:03 +0000

    Clean code

--- app/src/test/kotlin/adventOfCode2020/Utils.kt
+++ app/src/test/kotlin/adventOfCode2020/Utils.kt
@@ -7,7 +7,7 @@ fun getFileAsListOfLines(fileName: String) =
     object {}::class.java.getResourceAsStream(fileName).bufferedReader().lines().toList()
 
 fun listOfListsByBlankLine(testData: List<String>) =
-    (listOf(-1) + testData.mapIndexed { index, item -> if (item.isNullOrEmpty()) index else -1 }.filter { it != -1 } + testData.size)
+    (listOf(-1) + testData.mapIndexed { index, item -> if (item.isEmpty()) index else -1 }.filter { it != -1 } + testData.size)
     .windowed(2, 1)
     .map { (start, end) -> testData.subList(start + 1, end) }
     .map { i -> i.map { j -> j.split(" ") }.flatten() }
