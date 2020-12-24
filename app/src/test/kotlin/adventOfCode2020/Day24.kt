5be6bb0cf48f22d853e7449205e96883e3b93bfa
Graham Berks <graham@fatlazycat.com>
2020-12-24 09:29:33 +0000

    Day 24 P2 Done

--- app/src/test/kotlin/adventOfCode2020/Day24.kt
+++ app/src/test/kotlin/adventOfCode2020/Day24.kt
@@ -1,17 +1,16 @@
 package adventOfCode2020
 
 import kotlinx.collections.immutable.PersistentMap
-import kotlinx.collections.immutable.toPersistentMap
-import kotlinx.collections.immutable.*
+import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.collections.immutable.plus
 import org.junit.Test
-import java.lang.UnsupportedOperationException
 
 class Day24 {
     @Test
     fun testPart1() {
         val lines: List<String> = getFileAsListOfLines("/day24")
         val data = lines.map { processRow(it) }
-        val totalBlacks = keepBlacks(data)
+        val totalBlacks = keepBlacks(data).size
 
         assert(totalBlacks == 300)
     }
@@ -20,14 +19,116 @@ class Day24 {
     fun testPart1test() {
         val lines: List<String> = getFileAsListOfLines("/day24test")
         val data = lines.map { processRow(it) }
-        val totalBlacks = keepBlacks(data)
+        val totalBlacks = keepBlacks(data).size
 
         assert(totalBlacks == 10)
     }
 
-    private tailrec fun keepBlacks(data: List<List<Direction>>, current: PersistentMap<Pair<Int, Int>, Boolean> = persistentMapOf()): Int {
+    @Test
+    fun testPart2() {
+        val lines: List<String> = getFileAsListOfLines("/day24")
+        val data = lines.map { processRow(it) }
+        var pattern = keepBlacks(data).toMap()
+
+        repeat(100) {
+            pattern = processDay(pattern)
+        }
+
+        assert(pattern.size == 3466)
+    }
+
+    @Test
+    fun testPart2testSanity() {
+        val lines: List<String> = getFileAsListOfLines("/day24test")
+        val data = lines.map { processRow(it) }
+        val initialPattern = keepBlacks(data)
+        val afterDay1 = processDay(initialPattern)
+        val afterDay2 = processDay(afterDay1)
+        val afterDay3 = processDay(afterDay2)
+
+        assert(afterDay1.size == 15)
+        assert(afterDay2.size == 12)
+        assert(afterDay3.size == 25)
+    }
+
+    @Test
+    fun testPart2test() {
+        val lines: List<String> = getFileAsListOfLines("/day24test")
+        val data = lines.map { processRow(it) }
+        var pattern = keepBlacks(data).toMap()
+
+        repeat(100) {
+            pattern = processDay(pattern)
+        }
+
+        assert(pattern.size == 2208)
+    }
+
+    private fun processDay(data: Map<Pair<Int, Int>, Boolean>): Map<Pair<Int, Int>, Boolean> {
+        val bounds = getBounds(data)
+        val min = Pair(bounds.first.first - 2, bounds.first.second - 1)
+        val max = Pair(bounds.second.first + 2, bounds.second.second + 1)
+
+        return (min.second..max.second).map { y ->
+            val offset = if (y % 2 == 0) 0 else 1// we have a black tile
+
+            // we have a white tile
+            ((min.first - offset)..(max.first + offset) step 2).mapNotNull { x ->
+                val coord = Pair(x, y)
+                val adjacent = adjacentBlackTiles(coord, data)
+                val current = data[coord]
+
+                if (current == null) {
+                    // we have a white tile
+                    if (adjacent == 2)
+                        coord to true
+                    else
+                        null
+                } else {
+                    // we have a black tile
+                    if (adjacent == 0 || adjacent > 2)
+                        null
+                    else
+                        coord to true
+                }
+            }
+        }.flatten().toMap()
+    }
+
+    private fun adjacentBlackTiles(coord: Pair<Int, Int>, data: Map<Pair<Int, Int>, Boolean>): Int {
+        val ne = if(data[Pair(coord.first+1, coord.second+1)] != null) 1 else 0
+        val nw = if(data[Pair(coord.first-1, coord.second+1)] != null) 1 else 0
+        val se = if(data[Pair(coord.first+1, coord.second-1)] != null) 1 else 0
+        val sw = if(data[Pair(coord.first-1, coord.second-1)] != null) 1 else 0
+        val e = if(data[Pair(coord.first+2, coord.second)] != null) 1 else 0
+        val w = if(data[Pair(coord.first-2, coord.second)] != null) 1 else 0
+
+        return ne + nw + se + sw + e + w
+    }
+
+    private fun getBounds(data: Map<Pair<Int, Int>, Boolean>): Pair<Pair<Int, Int>, Pair<Int, Int>> {
+        var min = data.keys.fold(Pair(0,0)){ acc, p ->
+            Pair(Integer.min(acc.first, p.first), Integer.min(acc.second, p.second))
+        }
+
+        var max = data.keys.fold(Pair(1, 1)){ acc, p ->
+            Pair(Integer.max(acc.first, p.first), Integer.max(acc.second, p.second))
+        }
+
+        if(min.first % 2 != 0)
+            min = Pair(min.first-1, min.second)
+        if(max.first % 2 != 0)
+            max = Pair(max.first+1, max.second)
+
+        return Pair(min, max)
+    }
+
+
+    private tailrec fun keepBlacks(data: List<List<Direction>>,
+                                   current: PersistentMap<Pair<Int, Int>, Boolean> = persistentMapOf())
+    : PersistentMap<Pair<Int, Int>, Boolean> {
         return if(data.isEmpty())
-            return current.size
+            return current
         else {
             val coord = getCoords(data.first())
             val tile = current[coord]
