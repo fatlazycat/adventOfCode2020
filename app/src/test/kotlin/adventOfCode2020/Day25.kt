63e4aa7340867a35676f645adc532f528be05267
Graham Berks <graham@fatlazycat.com>
2020-12-25 20:13:27 +0000

    Done D25 P1

---
+++ app/src/test/kotlin/adventOfCode2020/Day25.kt
@@ -0,0 +1,29 @@
+package adventOfCode2020
+
+import org.junit.Test
+
+class Day25 {
+
+    private val cardPublicKey = 18356117L
+    private val doorPublicKey = 5909654L
+    private val cardPublicKeyTest = 5764801L
+    private val doorPublicKeyTest = 17807724L
+
+    @Test
+    fun testPart1test() {
+        assert(findLoopSize(cardPublicKeyTest) == 8)
+        assert(findLoopSize(doorPublicKeyTest) == 11)
+        assert(findLoopSize(cardPublicKey) == 3974372)
+        assert(findLoopSize(doorPublicKey) == 8623737)
+
+        assert(calc(doorPublicKeyTest, 8) == 14897079L)
+        assert(calc(cardPublicKeyTest, 11) == 14897079L)
+        assert(calc(doorPublicKey, 3974372) == 16902792L)
+        assert(calc(cardPublicKey, 8623737) == 16902792L)
+    }
+
+    private fun findLoopSize(target: Long): Int =
+        generateSequence(1L) { loopSize -> calc(7, loopSize) }.indexOf(target)
+
+    private fun calc(subjectNumber: Long, loopSize: Long): Long = (subjectNumber * loopSize) % 20201227L
+}
